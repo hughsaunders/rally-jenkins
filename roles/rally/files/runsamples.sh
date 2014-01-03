d6951ae7b49d4fc594aaa8840c91ac9a8a0c2827
Hugh Saunders <hugh@wherenow.org>
2014-01-03 12:44:07 +0000

    keep all temp task jsons

--- roles/rally/files/runsamples.sh
+++ roles/rally/files/runsamples.sh
@@ -3,9 +3,10 @@ iid=$(nova image-list |awk '$4 ~ /precise/{print $2}')
 did=$(openstack-rally deployment list|awk '/dummy/{print $2}'|tail -n1)
 die(){ echo $@; exit 1; }
 for task in doc/samples/tasks/nova/*; do
-    jq '.[][]["args"]={image_id: "'$iid'", flavor_id:10}' <$task >/tmp/task
-    openstack-rally --verbose task start --deploy-id="$did" --task=/tmp/task
+    tmptask=/tmp/$(basename $task)
+    jq '.[][]["args"]={image_id: "'$iid'", flavor_id:10}' <$task >$tmptask
+    openstack-rally --verbose task start --deploy-id="$did" --task=$tmptask
     tuuid=$(openstack-rally task list |awk '$2 ~ /-/{tuuid=$2}; END{print tuuid}')
-    openstack-rally task results $tuuid |jq . || die "invalid jason returned by $task: $(cat /tmp/task)"
-    openstack-rally task detailed $tuuid | grep finished || die "Task failed: $task: $(cat /tmp/task)"
+    openstack-rally task results $tuuid |jq . || die "invalid jason returned by $task: $(cat $tmptask)"
+    openstack-rally task detailed $tuuid | grep finished || die "Task failed: $task: $(cat $tmptask)"
 done
