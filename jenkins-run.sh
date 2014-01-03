52d0cd64f19faa58c607ac278c6162d3f1e04d2f
Hugh Saunders <hugh@wherenow.org>
2014-01-03 20:34:04 +0000

    need to run launch tag before destroy

--- jenkins-run.sh
+++ jenkins-run.sh
@@ -9,4 +9,4 @@ cp -r ~jenkins/jobscripts/* .
 sed -i -e 's+ansible_python_interpreter=+ansible_python_interpreter='$VIRTUAL_ENV/bin/python'+' inventory-local/hosts
 cat inventory-local/hosts
 ansible-playbook bootstrap.yml -i inventory-local/ -e region=LON -e buildid="$BUILD_TAG" -e keypair=jenkins -vvvv --private-key=id_jenkins -t launch,deploy,rally
-ansible-playbook bootstrap.yml -i inventory-local/ -e region=LON -e buildid="$BUILD_TAG" -e keypair=jenkins -vvvv --private-key=id_jenkins -t destroy
+ansible-playbook bootstrap.yml -i inventory-local/ -e region=LON -e buildid="$BUILD_TAG" -e keypair=jenkins -vvvv --private-key=id_jenkins -t launch,destroy
