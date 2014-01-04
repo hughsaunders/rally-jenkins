d4bc93073852e3edff0596e0874f3e798325125e
Hugh Saunders <hugh@wherenow.org>
2014-01-04 15:06:14 +0000

    collect devstack logs

--- jenkins-run.sh
+++ jenkins-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
+mkdir logs
 git clone https://github.com/hughsaunders/rally-jenkins&
 virtualenv .venv
 source .venv/bin/activate
@@ -17,6 +18,5 @@ pip install ansible pyrax rackspace-novaclient
 cd rally-jenkins
 cp -r ~jenkins/jobscripts/* .
 sed -i -e 's+ansible_python_interpreter=+ansible_python_interpreter='$VIRTUAL_ENV/bin/python'+' inventory-local/hosts
-cat inventory-local/hosts
 ansible-playbook bootstrap.yml -i inventory-local/ -e region=LON -e buildid="$BUILD_TAG" -e keypair=jenkins -vvvv --private-key=id_jenkins -t launch,deploy,rally
 ansible-playbook bootstrap.yml -i inventory-local/ -e region=LON -e buildid="$BUILD_TAG" -e keypair=jenkins -vvvv --private-key=id_jenkins -t launch,destroy
