5558799ea1544f18814fe2fcb2d1539b7af6fd11
Hugh Saunders <hugh@wherenow.org>
2014-01-04 20:15:09 +0000

    actually use resumedevstack role

--- integration-only-template.sh
+++ integration-only-template.sh
@@ -2,5 +2,5 @@
 source lib.sh
 setup
 setup_ansible
-run_playbook launch,rally -e image=$(cat ~jenkins/jobscripts/devstack_image_id)
+run_playbook launch,resumedevstack,rally -e image=$(cat ~jenkins/jobscripts/devstack_image_id)
 run_playbook launch,destroy
