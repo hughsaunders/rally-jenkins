114e6710521d5daae96db6a41966d602ba1c7334
Hugh Saunders <hugh@wherenow.org>
2014-01-04 20:13:35 +0000

    actually add resumedevstack role anddelete old devstack images

--- devstack-only.sh
+++ devstack-only.sh
@@ -11,4 +11,9 @@ nova image-create $instance_name $instance_name --poll
 image_id=$(nova image-list |awk '/'$instance_name'/{print $2}')
 echo $image_id > ~/jobscripts/devstack_image_id
 
+# Delete old images created by this job
+nova image-list |awk '/'$image_id'/{next}; /'$JOB_NAME'/{print $2}'|while read iid; do
+    nova image-delete $iid
+done
+
 run_playbook launch,destroy
