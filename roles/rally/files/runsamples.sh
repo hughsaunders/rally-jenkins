d9d5ba7fd5cf3da4ceb4b59a80aa280848f8fbea
Hugh Saunders <hugh@wherenow.org>
2014-01-04 18:12:58 +0000

    update args without obliterating other args

--- roles/rally/files/runsamples.sh
+++ roles/rally/files/runsamples.sh
@@ -4,7 +4,7 @@ did=$(openstack-rally deployment list|awk '/dummy/{print $2}'|tail -n1)
 die(){ echo $@; exit 1; }
 for task in doc/samples/tasks/nova/*; do
     tmptask=/tmp/$(basename $task)
-    jq '.[][]["args"]={image_id: "'$iid'", flavor_id:10}' <$task >$tmptask
+    jq '.[][]["args"]["image_id"]="'$iid'"|.[][]["args"]["flavor_id"]=10' <$task >$tmptask
     openstack-rally --verbose task start --deploy-id="$did" --task=$tmptask
     tuuid=$(openstack-rally task list |awk '$2 ~ /-/{tuuid=$2}; END{print tuuid}')
     openstack-rally task results $tuuid |jq . || die "invalid jason returned by $task: $(cat $tmptask)"
